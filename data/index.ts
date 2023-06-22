5e2552f124e07a5d88fdc2532072a888415ac3de
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-06-22 01:59:14 -0400

    Added mock data

---
+++ data/index.ts
@@ -0,0 +1,30 @@
+export const NEWS_DATA = {
+  articles: [
+    {
+      author: "PRNewswire",
+      title:
+        "Video Streaming Market size is to grow by USD 310.44 billion from 2022 to 2027, Growth is driven by the increased demand for encoders capable of supporting multiple broadcasting formats - Technavio",
+      description:
+        "NEW YORK, June 21, 2023 /PRNewswire/ -- The video streaming market size is set to grow by USD 310.44 billion from 2022 to 2027, progressing at a CAGR of 20.36% during the forecast period.",
+      url: "https://www.benzinga.com/pressreleases/23/06/n32958654/video-streaming-market-size-is-to-grow-by-usd-310-44-billion-from-2022-to-2027-growth-is-driven-by",
+      timestamp: [Object],
+      urlToImage:
+        "https://www.benzinga.com/next-assets/images/schema-image-default.png",
+      content:
+        "NEW YORK, June 21, 2023 /PRNewswire/ -- The video streaming market size is set to grow by USD 310.44 billion from 2022 to 2027, progressing at a CAGR of 20.36% during the forecast period.",
+    },
+    {
+      author: "Zacks Investment Research",
+      title: "IBM AI-Powered Tools to Enrich Wimbledon Digital Experience",
+      description:
+        "IBM introduces new AI-powered tools in Wimbledon to expand the digital experience to keep the fans entertained, engaged and informed throughout the championship.",
+      url: "https://www.zacks.com/stock/news/2111374/ibm-ai-powered-tools-to-enrich-wimbledon-digital-experience",
+      timestamp: [Object],
+      urlToImage:
+        "https://staticx-tuner.zacks.com/images/articles/main/3a/521.jpg",
+      content:
+        "IBM introduces new AI-powered tools in Wimbledon to expand the digital experience to keep the fans entertained, engaged and informed throughout the championship.",
+    },
+  ],
+  count: 2,
+};
