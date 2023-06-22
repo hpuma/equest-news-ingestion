2f77276778120cb00d2ee79cee66b32019a27776
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-06-22 02:07:32 -0400

    Removed redundant code

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,6 @@ async function main() {
   const encryptionKey = "secret";
   const encryptor = new Encryptor(encryptionKey);
 
-  // TEST EQUEST
-  // let newRecordByHash = await equestApi.getNewsRecordByHash("testhash123");
-  // console.log("findNewsRecordByHash", newRecordByHash.data);
-
-  // const alphavNews = await equestApi.getAlphavNews("ibm");
-  // console.log("getAlphavNews", alphavNews.data);
-
-  // const marketauxNews = await equestApi.getMarketauxNews("ibm");
-  // console.log("getMarketauxNews", marketauxNews.data);
-
-  // const everythingNews = await equestApi.getNewsEverything("ibm");
-  // console.log("getNewsEverything", everythingNews.data);
-
-  // TEST ENCRYPTOR
-  // const title = "This is a title that needs encrypting";
-  // const encrypted = encryptor.encrypt(title);
-  // console.log("🚀 ~ file: index.ts:21 ~ main ~ encrypted:", encrypted);
-  // const decrypted = encryptor.decrypt(encrypted);
-  // console.log("🚀 ~ file: index.ts:24 ~ main ~ decrypted:", decrypted);
   const { articles } = NEWS_DATA;
 
   let newArticles: any = [];
@@ -44,10 +25,7 @@ async function main() {
     articles: newArticles,
     count: newArticles.length,
   });
-  console.log(
-    "🚀 ~ file: index.ts:47 ~ main ~ recordsInserted:",
-    recordsInserted
-  );
+  console.log("🚀recordsInserted:", recordsInserted);
 }
 
 main();
