cb9f58670a1abde01bd3ce59d921a046cbd2b8c2
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-06-22 02:03:42 -0400

    Added separate testing environment

---
+++ testing/index.ts
@@ -0,0 +1,30 @@
+import { EquestApi } from "../equest";
+import { Encryptor } from "../encryptor";
+
+async function main() {
+  const equestApi = new EquestApi();
+  const encryptionKey = "secret";
+  const encryptor = new Encryptor(encryptionKey);
+
+  // TEST EQUEST
+  let newRecordByHash = await equestApi.getNewsRecordByHash("testhash123");
+  console.log("findNewsRecordByHash", newRecordByHash.data);
+
+  const alphavNews = await equestApi.getAlphavNews("ibm");
+  console.log("getAlphavNews", alphavNews.data);
+
+  const marketauxNews = await equestApi.getMarketauxNews("ibm");
+  console.log("getMarketauxNews", marketauxNews.data);
+
+  const everythingNews = await equestApi.getNewsEverything("ibm");
+  console.log("getNewsEverything", everythingNews.data);
+
+  // TEST ENCRYPTOR
+  const title = "This is a title that needs encrypting";
+  const encrypted = encryptor.encrypt(title);
+  console.log("🚀 ~ file: index.ts:21 ~ main ~ encrypted:", encrypted);
+  const decrypted = encryptor.decrypt(encrypted);
+  console.log("🚀 ~ file: index.ts:24 ~ main ~ decrypted:", decrypted);
+}
+
+main();
