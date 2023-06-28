6f07fafb0876c3628a9c8f45e6617df8db372c30
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-06-28 12:01:21 -0400

    Apply NewsProcessor to main

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,12 @@
-import { EquestApi } from "../equest";
-import { Encryptor } from "../encryptor";
 import { NEWS_DATA } from "../data";
+import { NewsProcessor } from "../news-processor";
 
 async function main() {
-  const equestApi = new EquestApi();
   const encryptionKey = "secret";
-  const encryptor = new Encryptor(encryptionKey);
+  const newsProcessor = new NewsProcessor(NEWS_DATA, encryptionKey);
 
-  const { articles, ticker } = NEWS_DATA;
-
-  let newArticles: any = [];
-
-  for (const article of articles) {
-    const { title } = article;
-    const createdHash = encryptor.encrypt(title.replace(" ", ""));
-
-    const { data } = await equestApi.getNewsRecordByHash(createdHash);
-    const newRecord = {
-      ...article,
-      hash: createdHash,
-      timestamp: undefined,
-      ticker,
-    };
-
-    if (!data) newArticles.push(newRecord);
-  }
-  console.log("🚀 ~ file: index.ts:27 ~ main ~ newArticles:", newArticles);
-  const { data: recordsInserted } = await equestApi.uploadNewsRecords({
-    articles: newArticles,
-    count: newArticles.length,
-  });
-  console.log("🚀recordsInserted:", recordsInserted);
+  await newsProcessor.processArticles();
+  await newsProcessor.uploadNewsRecords();
 }
 
 main();
