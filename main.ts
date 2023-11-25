d4d9ed5b94395fe5ef94a0b681647cef2c48f679
Henry Puma <37905046+hpuma@users.noreply.github.com>
2023-11-25 01:21:32 -0500

    News ingestion v2

--- main.ts
+++ main.ts
@@ -1,32 +1,45 @@
 import { NewsProcessor } from "./news-processor.class";
 import { EquestApi } from "./equest.class";
 
+interface TickerRecord {
+  symbol: string;
+  name: string;
+  sector: string;
+  country: string;
+}
+
 async function main() {
   const encryptionKey = "secret";
-
   const equestApi = new EquestApi();
 
-  const ticker = "IBM";
-  const [alpahvResponse, marketauxResponse, newsResponse] = await Promise.all([
-    equestApi.getNewsFromSource("alphav", ticker),
-    equestApi.getNewsFromSource("marketaux", ticker),
-    equestApi.getNewsFromSource("news", ticker),
-  ]);
+  const { records, count } = await equestApi.getTickerRecords();
+  console.log("🚀 TickerRecords => COUNT =>", count);
 
+  const alphav: any[] = [];
+  const marketaux: any[] = [];
+  const news: any[] = [];
+
+  for await (const record of records) {
+    const { symbol: ticker } = record;
+    const [alphavRes, marketauxRes, newsRes] = await Promise.all([
+      equestApi.getNewsFromSource("alphav", ticker),
+      equestApi.getNewsFromSource("marketaux", ticker),
+      equestApi.getNewsFromSource("news", ticker),
+    ]);
+    if (!alphavRes.message) alphav.push(alphavRes);
+    if (!marketauxRes.message) marketaux.push(marketauxRes);
+    if (!newsRes.message) news.push(newsRes);
+  }
   const totalArticlesDownloaded =
-    alpahvResponse.articles.length ??
-    0 + marketauxResponse.articles.length ??
-    0 + newsResponse.articles.lengt ??
-    0;
-
-  console.log(`🚀 TotalArticles => DOWNLOADED => ${totalArticlesDownloaded}`);
+    alphav.length ?? 0 + marketaux.length ?? 0 + news.length ?? 0;
+  console.log("🚀 TotalArticles => DOWNLOADED =>", totalArticlesDownloaded);
 
   const newsProcessor = new NewsProcessor(
     equestApi,
     {
-      alpahvResponse,
-      marketauxResponse,
-      newsResponse,
+      alphav,
+      marketaux,
+      news,
     },
     encryptionKey
   );
--- news-processor.class.ts
+++ news-processor.class.ts
@@ -2,7 +2,7 @@ import { EquestApi } from "./equest.class";
 import { Encryptor } from "./utils/encryptor.class";
 import { NEWS_DATA } from "./utils/data";
 type Source = {
-  [key: string]: typeof NEWS_DATA;
+  [key: string]: any[any];
 };
 
 export class NewsProcessor {
@@ -17,9 +17,9 @@ export class NewsProcessor {
 
   constructor(equestApi: EquestApi, data: any, encryptionKey: string) {
     this.newsSources = {
-      alphav: data.alpahvResponse,
-      marketaux: data.marketauxResponse,
-      news: data.newsResponse,
+      alphav: data.alphav,
+      marketaux: data.marketaux,
+      news: data.news,
     };
     this.equestApi = equestApi;
     this.encryptor = new Encryptor(encryptionKey);
@@ -28,47 +28,43 @@ export class NewsProcessor {
   async processArticles() {
     this.startTimer(true);
     const allNewsSources = Object.keys(this.newsSources);
-    const unprocessedArticles: any[] = [];
-
-    allNewsSources.forEach((source) =>
-      unprocessedArticles.push(this.processArticlesBySource(source))
-    );
-    await Promise.all(unprocessedArticles).then(() =>
-      this.printTimer(
-        "TotalArticles",
-        "PROCESSED",
-        NewsProcessor.totalProcessed,
-        true
-      )
-    );
+    for await (const source of allNewsSources)
+      await this.processArticlesBySource(source);
   }
 
   async processArticlesBySource(newsSource: string) {
-    this.startTimer();
+    const data = this.newsSources[newsSource];
 
-    const { articles = [], ticker = "" } = this.newsSources[newsSource];
-    if (!articles.length)
-      return this.printTimer(newsSource, `${ticker} [EMPTY]`, 0);
+    for await (const source of data) {
+      this.startTimer();
+      let currentProcessed = 0;
+      const { articles = [], ticker = "" } = source;
 
-    let currentProcessed = 0;
-    for (const article of articles) {
-      const filteredText = article.title.replace(" ", "");
-      const hash = this.encryptor.encrypt(filteredText);
+      if (!articles.length) {
+        this.printTimer(newsSource, `${ticker} [EMPTY]`, 0);
+        continue;
+      }
 
-      const { data } = await this.equestApi.getNewsRecordByHash(hash);
-      if (data) continue;
+      for (const article of articles) {
+        const filteredText = article.title.replace(" ", "");
+        const hash = this.encryptor.encrypt(filteredText);
 
-      this.newArticles.push({
-        ...article,
-        hash,
-        timestamp: undefined,
-        ticker,
-        newsSource,
-      });
-      NewsProcessor.totalProcessed += 1;
-      currentProcessed += 1;
+        const { data } = await this.equestApi.getNewsRecordByHash(hash);
+        if (data) continue;
+
+        this.newArticles.push({
+          ...article,
+          hash,
+          timestamp: undefined,
+          ticker,
+          newsSource,
+        });
+        NewsProcessor.totalProcessed += 1;
+        currentProcessed += 1;
+      }
+
+      this.printTimer(newsSource, `${ticker} PROCESSED`, currentProcessed);
     }
-    this.printTimer(newsSource, `${ticker} PROCESSED`, currentProcessed);
   }
 
   startTimer(isGlobal = false) {
@@ -91,16 +87,20 @@ export class NewsProcessor {
 
   async uploadNewsRecords() {
     this.startTimer();
+
     if (!this.newArticles.length)
       return this.printTimer("TotalArticles", "UPLOADED", 0);
 
-    const {
-      data: { insertedCount = 0 },
-    } = await this.equestApi.uploadNewsRecords({
+    const payload = {
       articles: this.newArticles,
       count: this.newArticles.length,
-    });
+    };
+
+    const {
+      data: { insertedCount = 0 },
+    } = await this.equestApi.uploadNewsRecords(payload);
 
     this.printTimer("TotalArticles", "UPLOADED", insertedCount);
+    this.newArticles = [];
   }
 }
